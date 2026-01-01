564bfeb84d4a289c31e340151ecf2f7271d6d1f4
Vallhup <ths1220ths@naver.com>
2026-01-02 00:26:43 +0900

    chore : initialize project

---
+++ Asio_Network_Module/pch.h
@@ -0,0 +1,17 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#define ASIO_STANDALONE
+#include "asio.hpp"
+
+using BYTE = unsigned char;
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
+
+#endif
